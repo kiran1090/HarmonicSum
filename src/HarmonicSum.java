7fadf5b221ea90dc33b90f75a6f66697fd6aeff6
kiran_a <iamkiranashok@gmail.com>
2023-01-10 14:25:46 +0530

    update

--- src/HarmonicSum.java
+++ src/HarmonicSum.java
@@ -1,18 +1,3 @@
-package javaexercises.flowcontrol;
-
-/**
- * Write a program called HarmonicSum to compute the sum of a harmonic series,
- * as shown below, where n=50000. The program shall compute the sum 
- * from left-to-right as well as from the right-to-left.
- * Obtain the difference between these two sums and explain the difference. 
- * 
- * Harmonic(n) = 1 + 1/2 + 1/3 + ... 1/n
- */
-
-/**
- *
- * @author User
- */
 public class HarmonicSum {
     
     public static void main(String[] args) {
@@ -21,23 +6,18 @@ public class HarmonicSum {
         HarmonicSum aHarmonicSum = new HarmonicSum();
         double sumL2R = aHarmonicSum.printLeftToRightSum(n);
         double sumR2L = aHarmonicSum.printRightToLeftSum(n);
-        System.out.printf("Difference: %.15f", (sumL2R - sumR2L));
-        System.out.println();
+        System.out.println("Difference: "+ (sumL2R - sumR2L));
+
     }
-    
-    /**
-     * Left-to-right harmonic sum.
-     * 
-     * @param int n 
-     */
+
     private double printLeftToRightSum(int n)
     {
         double sum = 0.0;
         for (int i = 1; i <= n; i++) {
             sum += (double) 1/i;
         }
-        System.out.printf("Left-to-right harmonic sum %.15f", sum);
-        System.out.println();
+        System.out.println("Left-to-right harmonic sum is "+ sum);
+
         
         return sum;
     }
@@ -48,8 +28,8 @@ public class HarmonicSum {
         for (int i = n; i >= 1; i--) {
             sum += (double) 1/i;
         }
-        System.out.printf("Right-to-left harmonic sum %.15f", sum);
-        System.out.println();
+        System.out.println("Right-to-left harmonic sum is "+ sum);
+
         
         return sum;
     }
